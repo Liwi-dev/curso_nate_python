d334ba6a3fe20353a03d72cb958647ed98a6bd86
liwi-dev <piter.formateos@gmail.com>
2023-01-07 11:48:30 -0300

    agregado libras a euros

---
+++ 02_libras_euros.py
@@ -0,0 +1,6 @@
+""" libras a euros """
+
+cantidad_libras = float(input("Cuantas libras quieres?: "))
+
+conversion_libras = cantidad_libras * 1.1340
+print("Necesitas {}".format(conversion_libras), "euros para el intercambio")

f7d3f8871c867792fe9f4d519e70df6b4e21a3e9
liwi-dev <piter.formateos@gmail.com>
2023-01-07 11:39:12 -0300

    primer programa

---
+++ 00_primer_programa.py
@@ -0,0 +1,22 @@
+# Reto 1
+
+# variables asignadas con input y datos tipo int
+numero_1 = int(input("Introduce tu primer numero!"))
+numero_2 = int(input("Introduce tu segundo numero!"))
+numero_3 = int(input("Introduce tu tercer numero!"))
+
+# Imprime el valor minimo
+print("Tu numero Minimo es ", min(numero_1, numero_2, numero_3))
+
+# Imprime el valor maximo
+print("Tu numero Maximo es ", max(numero_1, numero_2, numero_3))
+
+# solucion de nate
+numero_1 = int(input("Introduce tu primer numero: "))
+numero_2 = int(input("Introduce tu segundo numero: "))
+numero_3 = int(input("Introduce tu tercer numero: "))
+
+# Imprime el valor minimo
+print("Tu numero Minimo es: {}".format(min(numero_1, numero_2, numero_3)))
+# Imprime el valor maximo
+print("Tu numero Maximo es: {}".format(max(numero_1, numero_2, numero_3)))

81e75eb0666079123ead858117fbe605fea9ad37
liwi-dev <piter.formateos@gmail.com>
2023-01-07 11:46:14 -0300

    fahrenheit a celsius agregado

---
+++ 01_fahrenheit_celsius.py
@@ -0,0 +1,8 @@
+
+# introduce el valor en fahrenheit
+valor_fahrenheit = int(input("Intruduce la temperatura en Fahrenheit: "))
+
+# convierte el valor con la formula
+formula = (valor_fahrenheit - 32) / 1.8
+# imprime el resultado en pantalla
+print("La temperatura en Celsius es: {}".format(formula))

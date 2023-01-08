5f07420296a5447741f5eed709a055a43e46b284
liwi-dev <piter.formateos@gmail.com>
2023-01-07 22:13:04 -0300

    actualizado

--- 02_libras_euros.py
+++ 02_libras_euros.py
@@ -1,6 +1,16 @@
 """ libras a euros """
 
-cantidad_libras = float(input("Cuantas libras quieres?: "))
 
-conversion_libras = cantidad_libras * 1.1340
-print("Necesitas {}".format(conversion_libras), "euros para el intercambio")
+dolar_euro = 0.94
+libras_euros = 1.1340
+euro_libra = 0.88
+
+opcion = input("Que quieres convertir?: (libra o dolar a euro?)")
+opcion_2 = float(input("Cual es el monto de " + opcion + " a convertir: "))
+
+if opcion == "libra":
+    conversion_lib = opcion_2 * libras_euros
+    print("la conversion es ", conversion_lib, "euros")
+elif opcion == "dolar":
+    conversion_eur = dolar_euro * opcion_2
+    print("la conversion es ", conversion_eur, "euros")

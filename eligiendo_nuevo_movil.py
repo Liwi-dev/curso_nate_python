1b767b2314974226f8ddb78df4fe02f8dce53384
Piter Mancilla <piter.formateos@gmail.com>
2023-01-14 17:44:53 -0300

    Terminado el archivo y probado, mande la variable afuera para reutilizarla

--- eligiendo_nuevo_movil.py
+++ eligiendo_nuevo_movil.py
@@ -1,7 +1,8 @@
 ios_android = input("Que te gusta?: ios o android \n")
 
+tienes_dinero = input("Tienes dinero? (si/no) \n")
+
 if ios_android == "android":
-    tienes_dinero = input("Tienes dinero? (si/no) \n")
     if tienes_dinero == "no":
         print("Comprate un android chino de 100 pavos!")
     elif tienes_dinero == "si":
@@ -10,3 +11,10 @@ if ios_android == "android":
             print("Comprate el Google Pixel 7 Pro!")
         elif camara_movil == "no":
             print("Comprate un android calidad-precio")
+
+
+if ios_android == "ios":
+    if tienes_dinero == "si":
+        print("Comprate el iphone 14 Pro Max")
+    elif tienes_dinero == "no":
+        print("Comprate un iphone de segunda mano")

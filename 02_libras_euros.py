04db5444284c9c631237425a9fb64d7754735cfa
Piter Mancilla <piter.formateos@gmail.com>
2023-01-14 15:20:52 -0300

    modifique lineas del archivo ya que lo habia hecho diferente pero esta es la forma correcta

--- 02_libras_euros.py
+++ 02_libras_euros.py
@@ -3,14 +3,29 @@
 
 dolar_euro = 0.94
 libras_euros = 1.1340
-euro_libra = 0.88
 
-opcion = input("Que quieres convertir?: (libra o dolar a euro?)")
-opcion_2 = float(input("Cual es el monto de " + opcion + " a convertir: "))
 
-if opcion == "libra":
-    conversion_lib = opcion_2 * libras_euros
-    print("la conversion es ", conversion_lib, "euros")
-elif opcion == "dolar":
-    conversion_eur = dolar_euro * opcion_2
-    print("la conversion es ", conversion_eur, "euros")
+
+opcion = input("Que desea hace?\n"
+               "A - Convertir de dolar a euro\n"
+               "B - Convertir de euro a dolar\n"
+               "C - Convertir de libra a euro\n"
+               "D - Convertir de euro a libra\n")
+
+text_user = "Introduzca la cantidad {} a convertir: "
+
+if opcion == "A":
+    cantidad_de_dinero = float(input(text_user.format("dolares")))
+    print("la cantidad en euros es: {}".format(cantidad_de_dinero * dolar_euro))
+
+if opcion == "B":
+    cantidad_de_dinero = float(input(text_user.format("dolares")))
+    print("la cantidad en euros es: {}".format(cantidad_de_dinero * dolar_euro))
+
+if opcion == "C":
+    cantidad_de_dinero = float(input(text_user.format("dolares")))
+    print("la cantidad en euros es: {}".format(cantidad_de_dinero * dolar_euro))
+
+if opcion == "D":
+    cantidad_de_dinero = float(input(text_user.format("dolares")))
+    print("la cantidad en euros es: {}".format(cantidad_de_dinero * dolar_euro))

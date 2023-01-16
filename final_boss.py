e3fd2884b9710e352f942b23cda5810066a8dfc2
liwi-dev <piter.formateos@gmail.com>
2023-01-16 07:44:50 -0300

    actualizo archivo de final boss

--- final_boss.py
+++ final_boss.py
@@ -12,44 +12,45 @@ print("Juego de la mazmorra\n"
 opcion = input("[OPCION (A) - Puerta Semiabierta] | [OPCION (B) - Escotilla en el suelo]: ")
 
 if opcion == "A":
-      print("Entras en la puerta semiabierta y otro guardia te descubre, que haces?")
-      opcion = input("[OPCION (A) - Te haces el dormido] | [OPCION (B) - Sales corriendo hacia la siguiente puerta]: ")
+    print("Entras en la puerta semiabierta y otro guardia te descubre, que haces?")
+    opcion = input("[OPCION (A) - Te haces el dormido] | [OPCION (B) - Sales corriendo hacia la siguiente puerta]: ")
 
-      if opcion == "A":
-            print("El guardia te atrapa, te encierran en una celda de maxima seguridad\nFIN")
-      elif opcion == "B":
-            print("Despues de esa puerta encuentras una rana mutante que te regala un puñal casero hecho con la pata de"
-                  "una mesa, lo aceptas?")
-            opcion = input("[OPCION (A] - Si | [OPCION (B) - No]: ")
+    if opcion == "A":
+        print("El guardia te atrapa, te encierran en una celda de maxima seguridad\nFIN")
+    elif opcion == "B":
+        print("Despues de esa puerta encuentras una rana mutante que te regala un puñal casero hecho con la pata de"
+              "una mesa, lo aceptas?")
+        opcion = input("[OPCION (A] - Si | [OPCION (B) - No]: ")
+
+        if opcion == "A":
+            print("Coges el pincho y avanzas, hay dos pasillos circulares, no ves el final de ninguno de los dos, uno"
+                  "esta a la derecha y el otro a la izquierda, cual tomas?")
+
+            opcion = input("[OPCION (A) - Izquierda] | [OPCION (B) - Derecha] ")
 
             if opcion == "A":
-                  print("Coges el pincho y avanzas, hay dos pasillos circulares, no ves el final de ninguno de los dos, uno"
-                        "esta a la derecha y el otro a la izquierda, cual tomas?")
-
-                  opcion = input("[OPCION (A) - Izquierda] | [OPCION (B) - Derecha] ")
-
-                  if opcion == "A":
-                        print("La habitacion se hace cada vez mas oscura, ves tan poco que caes en un agujero en el suelo con"
-                              "pinchos, mueres a las 2 horas de forma lenta y dolorosa \nFIN")
-                  elif opcion == "B":
-                        print("Encuentras la salida, en la puerta hay aparcado un Ferrari espacial, te montas, es tu dia de"
-                              " suerte, las llaves estan puestas, Huyes hacia el horizonte\nFIN")
-
+                print("La habitacion se hace cada vez mas oscura, ves tan poco que caes en un agujero en el suelo con"
+                      "pinchos, mueres a las 2 horas de forma lenta y dolorosa \nFIN")
             elif opcion == "B":
-                  print("La rana te devora, mueres en forma rapida, el veneno hace efecto de casi de manera instantanea.\nFIN")
-            else:
-                  print("No has elegido ninguna opcion, simplemente mueres.")
-      else:
+                print("Encuentras la salida, en la puerta hay aparcado un Ferrari espacial, te montas, es tu dia de"
+                      " suerte, las llaves estan puestas, Huyes hacia el horizonte\nFIN")
+
+        elif opcion == "B":
+            print(
+                "La rana te devora, mueres en forma rapida, el veneno hace efecto de casi de manera instantanea.\nFIN")
+        else:
             print("No has elegido ninguna opcion, simplemente mueres.")
+    else:
+        print("No has elegido ninguna opcion, simplemente mueres.")
 
 elif opcion == "B":
-      print("Caes en una zona inundada, el agua te llega hasta las rodillas, avanzas durante media hora y finalmente"
-            " llegas a una zona abierta, seca y con luz, parecen alcantarillas\nEncuentras un palo largo, parece"
-            " algo pesado, pero podria servir, lo coges?")
-      opcion = input("[OPCION (A) - SI] | [OPCION (B - NO)]: ")
+    print("Caes en una zona inundada, el agua te llega hasta las rodillas, avanzas durante media hora y finalmente"
+          " llegas a una zona abierta, seca y con luz, parecen alcantarillas\nEncuentras un palo largo, parece"
+          " algo pesado, pero podria servir, lo coges?")
+    opcion = input("[OPCION (A) - SI] | [OPCION (B - NO)]: ")
 
-      palo_en_inventario = False
+    palo_en_inventario = False
 
-      if opcion == "A":
-            print("Coges el palo")
-            palo_en_inventario = True
+    if opcion == "A":
+        print("Coges el palo")
+        palo_en_inventario = True

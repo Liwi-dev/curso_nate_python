44e34a7530ca9c00ec9d685ab19412b8d3e94ef6
liwi-dev <piter.formateos@gmail.com>
2023-01-20 06:39:30 -0300

    correccion de de advertencias

--- ejemplo_final_boss.py
+++ ejemplo_final_boss.py
@@ -2,10 +2,10 @@ import random
 
 titulo = "El laberinto Mágico"
 print("\n" + titulo + "\n" + "·" * len(titulo) + "\n"
-    "Había una vez un guerrero llamado Rafael que estaba atrapado en un laberinto mágico.Cada vez que tomaba un camino,""\n"
-    "se encontraba con una nueva bifurcación.Rafael tenía que tomar decisiones rápidas para avanzar a través del""\n"
-    "laberinto y derrotar al dragón final que guardaba el tesoro.""\n""\n"
-    "En uno de los primeros caminos, Rafael se encontró con una bifurcación que lo llevaba a la derecha o a la izquierda.""\n")
+                                                 "Había una vez un guerrero llamado Rafael que estaba atrapado en un laberinto mágico.Cada vez que tomaba un camino,""\n"
+                                                 "se encontraba con una nueva bifurcación.Rafael tenía que tomar decisiones rápidas para avanzar a través del""\n"
+                                                 "laberinto y derrotar al dragón final que guardaba el tesoro.""\n""\n"
+                                                 "En uno de los primeros caminos, Rafael se encontró con una bifurcación que lo llevaba a la derecha o a la izquierda.""\n")
 
 opcion = input("[OPCION (A) - Camino de la izquierda | OPCION (B) - Camino de la derecha]:""\n")
 
@@ -18,20 +18,21 @@ if opcion == "A":  # Se fue a la izquierda
 
     if opcion == "A":  # se fue hacia arriba
 
-        print("Esta vez, Rafael decidió ir hacia arriba y se encontró con una puerta secreta que lo llevó a un atajo""\n"
-        "hacia el final del laberinto. Rafael agradeció su buena suerte y salio sano y salvo.""\n""FIN""\n")
+        print(
+            "Esta vez, Rafael decidió ir hacia arriba y se encontró con una puerta secreta que lo llevó a un atajo""\n"
+            "hacia el final del laberinto. Rafael agradeció su buena suerte y salio sano y salvo.""\n""FIN""\n")
         exit()
     else:  # se fue hacia abajo
 
         print("HAS MUERTO QUEMADO POR LA LAVA")
         exit()
-else:  #eligio el camino de la derecha
+else:  # eligio el camino de la derecha
 
     print("Después de pensar por un momento, Rafael decidió ir a la derecha y se encontró con una trampa de arañas.""\n"
           "Tuvo que decidir si luchar contra las arañas o perder un valioso tiempo y energía rodeando el area.""\n")
 
     opcion = input("[OPCION (A) - Lucha contra las arañas | OPCION (B) - Rodea el area]:""\n")
-    if opcion == "A":  #Lucha contra las arañas
+    if opcion == "A":  # Lucha contra las arañas
         print("Tuvo que luchar contra las arañas y perdió la vida""\n")
         print("HAS MUERTO DEVORADO POR LAS ARAÑAS")
         exit()
@@ -43,7 +44,7 @@ else:  #eligio el camino de la derecha
     opcion = input("[OPCION (A) - Coge la espada | OPCION (B) - Continua sin ella]:""\n")
     espada_en_inventario = False  # LA ESPADA NO ESTA EN EL INVENTARIO POR AHORA
 
-    if  opcion == "A":  # COGE LA ESPADA
+    if opcion == "A":  # COGE LA ESPADA
         print("Coges la espada")
         espada_en_inventario = True
     elif opcion == "B":  # NO COGE LA ESPADA
@@ -55,7 +56,7 @@ else:  #eligio el camino de la derecha
     numero_fauno = random.randint(1, 50)
     print("Mientras avanzaba por el laberinto, Rafael se encontró con un fauno que le hizo resolver un acertijo""\n"
           "matemático. Rafael tuvo que adivinar el resultado de sumar {} + 25.".format(numero_fauno))
-    opcion  = int(input("¿Cual es el resultado? "))
+    opcion = int(input("¿Cual es el resultado? "))
 
     pocion = False
     if opcion == 25 + numero_fauno:  # ACIERTA
@@ -67,11 +68,11 @@ else:  #eligio el camino de la derecha
         print("HAS MUERTO ENVENENADO POR EL FAUNO")
     print("Finalmente, Rafael llegó al final del laberinto y se encuentra al dragón.""\n")
 
-    opcion  = input("[OPCION (A) - Te haces bola | OPCION (B) - Te enfrentas al dragon]:")
+    opcion = input("[OPCION (A) - Te haces bola | OPCION (B) - Te enfrentas al dragon]:")
 
     if opcion == "A":  # NO SE ENFRENTA AL DRAGON
         print("El dragon te mira desconcertado y te calcina con una bola de fuego""\n""HAS MUERTO CALCINADO")
-    elif opcion == "B" and espada_en_inventario == True and pocion == True:  #TIENESTODO
+    elif opcion == "B" and espada_en_inventario == True and pocion == True:  # TIENESTODO
         print("Rafael llegó al final del laberinto y se enfrentó al dragón. La espada y la poción mágica le dieron""\n"
               "una ventaja en la batalla y Rafael logró derrotar al dragón y obtener el tesoro. Rafael agradeció su""\n"
               "determinación y su habilidad para superar los obstáculos del camino de la derecha, así como su""\n"

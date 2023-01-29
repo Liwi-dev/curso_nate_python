64cd4a73f650852b5caacd59f5c41cd751832a34
liwi-dev <piter.formateos@gmail.com>
2023-01-29 04:46:01 -0300

    coorrecion de vida squirtle, añadido input para continuar

--- 10_combate_pokemon.py
+++ 10_combate_pokemon.py
@@ -15,16 +15,18 @@ while vida_pikachu > 0 and vida_squirtle > 0:
         vida_squirtle -= 10
     else:
         print("Pikachu ha usado ¡ONDA TRUENO!")
-        vida_squirtle -= -11
+        vida_squirtle -= 11
 
     print("La vida de Pikachu es: {}, La vida de Squirtle es: {}".format(vida_pikachu, vida_squirtle))
+    input("Enter para continuar...""\n\n")
+
 
     # Turno Squirtle
     print("Turno de Squirtle")
     ataque_squirtle = None
 
     while ataque_squirtle != "P" and ataque_squirtle != "A" and ataque_squirtle != "B":
-        ataque_squirtle = input("¿Que ataque quieres realizar? [P]Placaje, [A]Pistola Agua, [B]Burbuja")
+        ataque_squirtle = input("¿Que ataque quieres realizar? [P]Placaje, [A]Pistola Agua, [B]Burbuja""\n\n")
 
     if ataque_squirtle == "P":
         vida_pikachu -= 10
@@ -37,6 +39,8 @@ while vida_pikachu > 0 and vida_squirtle > 0:
         print("Squirtle ha usado ¡BURBUJA!")
 
     print("La vida de Pikachu es: {}, La vida de Squirtle es: {}".format(vida_pikachu, vida_squirtle))
+    input("Enter para continuar...""\n\n")
+
 
 if vida_pikachu > vida_squirtle:
     print("¡Ha ganado Pikachu!")

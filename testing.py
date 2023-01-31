bbad76c0ea52ff2ee84400b941faa1c9aaeec4be
liwi-dev <piter.formateos@gmail.com>
2023-01-31 05:56:20 -0300

    se ha modificado para tener barras de vida

--- testing.py
+++ testing.py
@@ -1,34 +1,32 @@
 from random import randint
 
-vida_total_pikachu = 80
-vida_total_squirtle = 90
+vida_pikachu = 80
+vida_squirtle = 90
 
-vida_pikachu = vida_total_pikachu
-vida_squirtle = vida_total_squirtle
+
+def dibujar_barra_vida(nombre, vida, vida_total):
+    barra_vida = "#" * int(vida / vida_total * 20)
+    falta_vida = "" * (20 - len(barra_vida))
+    print("{}: [{}{}] {}/{}".format(nombre, barra_vida, falta_vida, vida, vida_total))
 
 
 while vida_pikachu > 0 and vida_squirtle > 0:
-    # continua el combate
+    # Continua el combate
 
     # Turno Pikachu
     print("Turno de Pikachu")
     ataque_pikachu = randint(1, 2)
     if ataque_pikachu == 1:
-        #Bola voltio
+        # Bola Voltio
         print("Pikachu ha usado: ¡BOLA VOLTIO!")
         vida_squirtle -= 10
     else:
         print("Pikachu ha usado ¡ONDA TRUENO!")
         vida_squirtle -= 11
 
-
-    barra_vida = "#" * int(vida_pikachu / vida_total_pikachu * 20)
-    barra_vida = "#" * int(vida_squirtle / vida_total_squirtle * 20)
-    falta_vida = "" * (20 - len(barra_vida))
-    print("{}: [{}{}] {}/{}".format("Pikachu", barra_vida, falta_vida, vida_pikachu, vida_total_pikachu))
-    print("{}: [{}{}] {}/{}".format("Squirtle", barra_vida, falta_vida, vida_squirtle, vida_total_squirtle))
+    dibujar_barra_vida("Pikachu", vida_pikachu, 80)
+    dibujar_barra_vida("Squirtle", vida_squirtle, 90)
     input("Enter para continuar...""\n\n")
-
 
     # Turno Squirtle
     print("Turno de Squirtle")
@@ -47,16 +45,11 @@ while vida_pikachu > 0 and vida_squirtle > 0:
         vida_pikachu -= 9
         print("Squirtle ha usado ¡BURBUJA!")
 
-    barra_vida = "#" * int(vida_pikachu / vida_total_pikachu * 20)
-    barra_vida = "#" * int(vida_squirtle / vida_total_squirtle * 20)
-    falta_vida = "-" * (20 - len(barra_vida))
-    print("{}: [{}{}] {}/{}".format("Pikachu", barra_vida, falta_vida, vida_pikachu, vida_total_pikachu))
-    print("{}: [{}{}] {}/{}".format("Squirtle", barra_vida, falta_vida, vida_squirtle, vida_total_squirtle))
+    dibujar_barra_vida("Pikachu", vida_pikachu, 80)
+    dibujar_barra_vida("Squirtle", vida_squirtle, 90)
     input("Enter para continuar...""\n\n")
-
 
 if vida_pikachu > vida_squirtle:
     print("¡Ha ganado Pikachu!")
 else:
     print("¡Ha ganado Squirtle!")
-

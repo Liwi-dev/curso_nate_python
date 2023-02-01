499c9d8f541b3138904b90292fd9600cb1c40361
liwi-dev <piter.formateos@gmail.com>
2023-02-01 07:49:47 -0300

    tarea casi terminada

--- 11_lista_de_compra.py
+++ 11_lista_de_compra.py
@@ -1,7 +1,8 @@
-
 lista_compra = []
 
 opcion = None
+
+
 while opcion != "Q":
     opcion = input("¿Que desea comprar? [(Q) para salir]\n")
     if opcion == "Q":
@@ -12,6 +13,9 @@ while opcion != "Q":
         lista_compra.append(opcion)
         opcion = input("Estas seguro que lo quieres añadir? (S)Si/(N)No\n")
         if opcion == "S":
-           print("Se ha añadido {} a la lista".format(lista_compra))
-
-print(lista_compra)
+            print("Se ha añadido {} a la lista".format(lista_compra[-1]))
+        elif opcion == "N":
+            print("No añadimos {} a la lista".format(lista_compra[-1]))
+            lista_compra.pop(-1)
+    elif opcion in lista_compra:
+        print("{} ya esta en la lista!".format(str(lista_compra)))

74cb719966c9811e5c3ee36ab18647d12c26f7d9
liwi-dev <piter.formateos@gmail.com>
2023-02-02 06:14:53 -0300

    archivo subido

---
+++ 14_for_ej1.py
@@ -0,0 +1,14 @@
+# ejemplo de nate
+
+texto_usuario = "Hola, me llamo Nate. ¿Tu como te llamas?"
+espacios_texto = " "
+puntos_texto = "."
+comas = ","
+
+# output esperado
+espacios = 6
+puntos = 1
+comas = 1
+
+for letra in texto_usuario:
+    if letra in espacios_texto:

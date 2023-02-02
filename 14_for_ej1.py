627b42ff5904a62a3fbc7e09f1a7294b8f8295e8
liwi-dev <piter.formateos@gmail.com>
2023-02-02 06:24:04 -0300

    ejercicio terminado y funcional

--- 14_for_ej1.py
+++ 14_for_ej1.py
@@ -1,14 +1,28 @@
 # ejemplo de nate
 
+# output esperado
+# espacios = 6
+# puntos = 1
+# comas = 1
+
 texto_usuario = "Hola, me llamo Nate. ¿Tu como te llamas?"
+
 espacios_texto = " "
 puntos_texto = "."
-comas = ","
+comas_texto= ","
 
-# output esperado
-espacios = 6
-puntos = 1
-comas = 1
+espacios_encontrados = 0
+puntos_encontrados = 0
+comas_encontrados = 0
 
 for letra in texto_usuario:
     if letra in espacios_texto:
+        espacios_encontrados += 1
+    elif letra in puntos_texto:
+        puntos_encontrados += 1
+    elif letra in comas_texto:
+        comas_encontrados += 1
+
+print("Espacios encontrados: {}".format(espacios_encontrados))
+print("Puntos encontrados: {}".format(puntos_encontrados))
+print("Comas encontrados: {}".format(comas_encontrados))

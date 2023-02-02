bac31688462a664580cf30fa987d784af59bc855
liwi-dev <piter.formateos@gmail.com>
2023-02-02 11:53:41 -0300

    agregada solucion de nate al ejercicio 1

--- 16_for_ej3.py
+++ 16_for_ej3.py
@@ -10,7 +10,16 @@
 2 x 8 = 2
 2 x 9 = 2
 2 x 10 = 2
+
+solucion nate
+
+numero = int(input("Numero a multiplicar: "))
+
+for n in range(1, 11)
+    print("{} x {} = {}".format(numero, n, n * numero))
+
 """
+
 print("Introduce un numero y te dire su tabla de multiplicar\n")
 numero_usuario = int(input("Introduzca tu numero: "))
 

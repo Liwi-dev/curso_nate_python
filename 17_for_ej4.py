f2d7f0620477ed56cbe3f5e59f8ff1b0556292c5
liwi-dev <piter.formateos@gmail.com>
2023-02-07 00:45:19 -0300

    coreccion de nate y aprendizaje un poco mas avanzado

--- 17_for_ej4.py
+++ 17_for_ej4.py
@@ -8,6 +8,20 @@ numero_mas_pequeno = 1
 numero_mas_grande = 6
 
 """
+# Solucion de Nate
+
+
+numeros_introducidos = input("introduzca los numeros separados por coma: ")  # 1,2,3,4,5,6,7,8,9
+numeros_usuario = [int(numero) for numero in numeros_introducidos.split(",")]  # List comprehension #for express
+# lo que hace split es separar el string por muchas comas
+numero_pequeno = min(numeros_usuario)
+numero_grande = max(numeros_usuario)
+
+print("Este es tu numero peuqeño: {}""\n"
+      "Este es tu numero grande: {}".format(numero_pequeno, numero_grande))
+
+# mi solucion
+"""
 print("Agregando numeros a una lista")
 
 numerous_usuario = []
@@ -20,4 +34,4 @@ while input("¿Quiere añadir mas numeros? [si]/[no] ") == "si":
 else:
     print("Tu numero mas pequeño es: {}".format(min(numerous_usuario)))
     print("Tu numero mas grande es: {}".format(max(numerous_usuario)))
-
+"""

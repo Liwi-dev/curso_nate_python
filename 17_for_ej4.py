d53c962db2da81740d5690b414f1822ee167d081
Liwi-dev <piter.formateos@gmail.com>
2023-02-27 18:33:57 -0300

    termiando el archivo 17 y agregado el 18

--- 17_for_ej4.py
+++ 17_for_ej4.py
@@ -14,11 +14,27 @@ numero_mas_grande = 6
 numeros_introducidos = input("introduzca los numeros separados por coma: ")  # 1,2,3,4,5,6,7,8,9
 numeros_usuario = [int(numero) for numero in numeros_introducidos.split(",")]  # List comprehension #for express
 # lo que hace split es separar el string por muchas comas
+
+numero_chico = numeros_usuario[0]
+numero_grande = numeros_usuario[0]
+
+""" [1:] eso es un filtrado de lista, basicamente le estoy diciendo que filtre la lista desde el 1 y no desde 0 """
+for numero in numeros_usuario[1:]:
+      if numero_chico > numero:
+            numero_chico = numero
+      if numero_grande < numero:
+            numero_grande = numero
+
+print("Numero Grande: {}, Numero Pequeño: {}".format(numero_grande, numero_chico))
+
+# solucion mia facil
+"""
 numero_pequeno = min(numeros_usuario)
 numero_grande = max(numeros_usuario)
 
 print("Este es tu numero peuqeño: {}""\n"
       "Este es tu numero grande: {}".format(numero_pequeno, numero_grande))
+"""
 
 # mi solucion
 """

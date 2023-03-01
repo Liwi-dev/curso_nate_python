fde85f1cd930f0c61072527d01fcab52cd833265
Liwi-dev <piter.formateos@gmail.com>
2023-03-01 18:01:59 -0300

    mapa del juego terminado

--- 18_maze.py
+++ 18_maze.py
@@ -1,8 +1,22 @@
+# Maze Game
 """Variables constantes"""
 POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
-# ---------------
+""" --------------- """
 
 my_position = [3, 1]
+
+""" Mapa del juego """
+print("+" + "-" * MAP_WIDTH * 3 + "+")
+
+for coordinate_y in range(MAP_HEIGHT):
+    print("|", end="")
+    for coordinate_x in range(MAP_WIDTH):
+        print("   ", end="")
+    print("|")
+
+print("+" + "-" * MAP_WIDTH * 3 + "+")
+""" --------------------------------- """
+

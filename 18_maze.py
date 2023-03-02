f52a66cffccb14bc808fb258dee870715dadd932
Liwi-dev <piter.formateos@gmail.com>
2023-03-01 21:21:57 -0300

    terminado el ejercicio pero falta el teleport

--- 18_maze.py
+++ 18_maze.py
@@ -1,5 +1,6 @@
 # Maze Game
 import readchar
+import os
 
 """Variables constants"""
 POS_X = 0
@@ -8,27 +9,39 @@ MAP_WIDTH = 20
 MAP_HEIGHT = 15
 """ --------------- """
 
+my_position = [6, 3]
 
-my_position = [3, 1]
-my_player = " @ "
+while True:
+    """ Draw Map """
+    print("+" + "-" * MAP_WIDTH * 3 + "+")
 
-""" Draw Map """
-print("+" + "-" * MAP_WIDTH * 3 + "+")
+    for coordinate_y in range(MAP_HEIGHT):
+        print("|", end="")
+        for coordinate_x in range(MAP_WIDTH):
+            if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
+                print(" @ ", end="")
+            else:
+                print("   ", end="")
+        print("|")
 
-for coordinate_y in range(MAP_HEIGHT):
-    print("|", end="")
-    for coordinate_x in range(MAP_WIDTH):
-        if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
-            print(my_player, end="")
-        else:
-            print("   ", end="")
-    print("|")
+    print("+" + "-" * MAP_WIDTH * 3 + "+")
+    """ --------------------------------- """
 
-print("+" + "-" * MAP_WIDTH * 3 + "+")
-""" --------------------------------- """
+    """ Ask user where he wants to move """
 
-""" Ask user where he wants to move """
+    # direction = input("¿Donde te quieres mover? (Usa las teclas) [WASD]: ")
+    direction = readchar.readchar()
 
-#direction = input("¿Donde te quieres mover? (Usa las telcas) [WASD]: ")
-direction = readchar.readchar()
-print(direction.decode())
+    if direction == "w":
+        my_position[POS_Y] -= 1
+    elif direction == "s":
+        my_position[POS_Y] += 1
+    elif direction == "a" :
+        my_position[POS_X] -= 1
+    elif direction == "d":
+        my_position[POS_X] += 1
+    elif direction == "q":
+        break
+
+    os.system("cls")
+    """ ----------------------------------- """

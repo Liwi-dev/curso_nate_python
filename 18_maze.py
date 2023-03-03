4974fd8ff63bab2ab8f64644e9fa2e90360d9a16
liwi-dev <piter.formateos@gmail.com>
2023-03-03 02:20:35 -0300

    se añadieron objetos en el mapa

--- 18_maze.py
+++ 18_maze.py
@@ -10,6 +10,7 @@ MAP_HEIGHT = 15
 """ --------------- """
 
 my_position = [6, 3]
+map_objects = [[2, 3], [5, 6], [8, 9], [13, 8]]
 
 while True:
     """ Draw Map """
@@ -17,11 +18,18 @@ while True:
 
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end="")
+
         for coordinate_x in range(MAP_WIDTH):
+
+            char_to_draw = " "
+            for map_obj in map_objects:
+                if map_obj[POS_X] == coordinate_x and map_obj[POS_Y] == coordinate_y:
+                    char_to_draw = "*"
+
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
-                print(" @ ", end="")
-            else:
-                print("   ", end="")
+                char_to_draw = "@"
+
+            print(" {} ".format(char_to_draw), end="")
         print("|")
 
     print("+" + "-" * MAP_WIDTH * 3 + "+")
